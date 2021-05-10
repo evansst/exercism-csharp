9bdc94fc3861121ce5682bdec1d0af9224252fb5
Sam Evans <evansst@icloud.com>
2021-05-10 10:16:12 -0600

    Pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var upper = input.ToUpper();
+        var charArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        return charArray.All(upper.Contains);
+    }
+}
