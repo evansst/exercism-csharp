591990f9f2831faa21880c1b5f108b560a85a1bd
Sam Evans <evansst@icloud.com>
2021-05-10 16:06:50 -0600

    NucleotideCount adds solution

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml;
 
 public static class NucleotideCount
 {
+    private static Dictionary<char, int> _Seed =>
+        new Dictionary<char, int>
+        {
+            {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0},
+        };
+
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        try
+        {
+            return sequence.Aggregate(_Seed, (dictionary, c) =>
+            {
+                dictionary[c]++;
+                return dictionary;
+            });
+        }
+        catch (KeyNotFoundException _e)
+        {
+            throw new ArgumentException("Those aren't nucleotides!");
+        }
     }
 }
