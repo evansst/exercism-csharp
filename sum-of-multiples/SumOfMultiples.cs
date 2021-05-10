748a866a78abe087e0f9a26d48e16e940567c081
Sam Evans <evansst@icloud.com>
2021-05-10 10:50:34 -0600

    SumOfMultiples adds solution

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
-    public static int Sum(IEnumerable<int> multiples, int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int Sum(IEnumerable<int> multiples, int max) => Enumerable
+        .Range(1, max - 1)
+        .Where(n => multiples.Any(m => m != 0 && n % m == 0))
+        .Sum();
 }
