82263a3a49edb527e02a776935fa0e27ccf65d21
Sam Evans <evansst@icloud.com>
2021-05-10 13:09:32 -0600

    Hamming adds solution

--- hamming/Hamming.cs
+++ hamming/Hamming.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
-    public static int Distance(string firstStrand, string secondStrand)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int Distance(string firstStrand, string secondStrand) =>
+        firstStrand.Length == secondStrand.Length
+            ? firstStrand.Where((t, i) => t != secondStrand[i]).Count()
+            : throw new ArgumentException("Arguments must have the same length.");
 }
