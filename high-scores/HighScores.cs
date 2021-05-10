d3f4a98a8c9e4b9e763ea02c56c5182a3d635b8b
Sam Evans <evansst@icloud.com>
2021-05-10 11:56:16 -0600

    HighScores adds solution

--- high-scores/HighScores.cs
+++ high-scores/HighScores.cs
@@ -1,31 +1,17 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class HighScores
 {
-    public HighScores(List<int> list)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly List<int> _list;
 
-    public List<int> Scores()
-    {
-        throw new NotImplementedException();
-    }
+    public HighScores(List<int> list) => _list = list;
 
-    public int Latest()
-    {
-        throw new NotImplementedException();
-    }
+    public List<int> Scores() => _list;
 
-    public int PersonalBest()
-    {
-        throw new NotImplementedException();
-    }
+    public int Latest() => _list.Last();
 
-    public List<int> PersonalTopThree()
-    {
-        throw new NotImplementedException();
-    }
+    public int PersonalBest() => _list.Max();
+
+    public List<int> PersonalTopThree() => _list.OrderByDescending(i => i).Take(3).ToList();
 }
