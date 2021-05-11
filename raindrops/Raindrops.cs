a924d8c5bccaf5be095fceea8de52add085da57b
Sam Evans <evansst@icloud.com>
2021-05-11 08:55:35 -0600

    Raindrops adds solution

--- raindrops/Raindrops.cs
+++ raindrops/Raindrops.cs
@@ -1,9 +1,25 @@
-using System;
+using System.Collections.Generic;
 
 public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var drops = "";
+        if (number % 3 == 0)
+        {
+            drops += "Pling";
+        }
+
+        if (number % 5 == 0)
+        {
+            drops += "Plang";
+        }
+
+        if (number % 7 == 0)
+        {
+            drops += "Plong";
+        }
+
+        return drops != "" ? drops : number.ToString();
     }
 }
