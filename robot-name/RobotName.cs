28373e287fb318b1625155002ebd2a59a6470ccb
Sam Evans <evansst@icloud.com>
2021-05-11 18:12:06 -0600

    RobotName adds solution

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,17 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 
 public class Robot
 {
+    private string _name;
+    private static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+    private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static List<string> All = new List<string>();
+
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if(_name == null) Reset();
+            return _name;
         }
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        byte[] randomNumbers = new byte[5];
+        rng.GetNonZeroBytes(randomNumbers);
+        var firstLetter = alphabet[randomNumbers[0] % 26];
+        var secondLetter = alphabet[randomNumbers[1] % 26];
+        var one = randomNumbers[2] % 10;
+        var two = randomNumbers[3] % 10;
+        var three = randomNumbers[4] % 10;
+        _name = firstLetter.ToString() + secondLetter + one + two + three;
+        
+        if (All.Contains(_name)) Reset();
+        else All.Add(_name);
     }
 }
