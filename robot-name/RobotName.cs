ea53820c53190ff36b940a0f9d9c5d47648b631f
Sam Evans <evansst@icloud.com>
2021-05-11 18:28:02 -0600

    RobotName adds solution

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,36 +1,38 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 
 public class Robot
 {
-    private string _name;
     private static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
     private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static List<string> All = new List<string>();
+    private static readonly List<string> All = new List<string>();
 
-    public string Name
-    {
-        get
-        {
-            if(_name == null) Reset();
-            return _name;
-        }
-    }
+    public Robot() => Reset();
+
+    public string Name { get; private set; }
 
     public void Reset()
     {
-        byte[] randomNumbers = new byte[5];
-        rng.GetNonZeroBytes(randomNumbers);
-        var firstLetter = alphabet[randomNumbers[0] % 26];
-        var secondLetter = alphabet[randomNumbers[1] % 26];
-        var one = randomNumbers[2] % 10;
-        var two = randomNumbers[3] % 10;
-        var three = randomNumbers[4] % 10;
-        _name = firstLetter.ToString() + secondLetter + one + two + three;
+        var randomLetter1 = RandomLetter();
+        var randomLetter2 = RandomLetter();
+        var randomNumber = RandomNumber();
+        Name = $"{randomLetter1}{randomLetter2}{randomNumber:000}";
         
-        if (All.Contains(_name)) Reset();
-        else All.Add(_name);
+        if (All.Contains(Name)) Reset();
+        else All.Add(Name);
+    }
+
+    private static char RandomLetter()
+    {
+        byte[] randomLetter = new byte[1];
+        rng.GetBytes(randomLetter);
+        return alphabet[randomLetter[0] % 26];
+    }
+
+    private static int RandomNumber()
+    {
+        byte[] randomLetter = new byte[1];
+        rng.GetBytes(randomLetter);
+        return randomLetter[0] * 4 % 1000;
     }
 }
